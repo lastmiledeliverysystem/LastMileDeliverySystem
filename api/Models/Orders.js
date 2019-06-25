5e96938abb6d5f468dc4171647106992e5152c9f
Maha <mahaelgende@gmail.com>
2019-06-25 14:24:29 +0200

    order date

--- api/Models/Orders.js
+++ api/Models/Orders.js
@@ -3,26 +3,14 @@ const mongoose = require('mongoose');
 const orders = new mongoose.Schema({
   salesOrderNumber: mongoose.Schema.Types.ObjectId,
   robotId: mongoose.Schema.Types.ObjectId,
-  date: {
-    type: Date,
-    default: Date.now,
-  },
+  date: { type: Date, default: +new Date() + 2 * 60 * 60 * 1000 },
   status: {
     type: String,
-    required: false,
+    required: true,
   },
   quantity: Number,
-  items: [{
-    name:String,
-    category:String,
-    description:String,
-    price:Number,
-    unit:String,
-    vendorId: mongoose.Schema.Types.ObjectId,
-    
-  }],
-  // items: String,
-  customerId: { type: mongoose.Schema.Types.ObjectId },
+  itemId: mongoose.Schema.Types.ObjectId,
+  customerId: mongoose.Schema.Types.ObjectId,
   discountAmount: Number,
   discount: Number,
   shippingCharge: Number,
@@ -30,28 +18,39 @@ const orders = new mongoose.Schema({
   notes: String,
   address: {
     lat: Number,
-    long: Number,
+    mag: Number,
   },
   shippmentDate: {
     type: Date,
-    default: Date.now,
+    default: +new Date() + 2.5 * 60 * 60 * 1000,
   },
   deliveryDate: {
     type: Date,
-    default: Date.now,
+    default: +new Date() + 3 * 60 * 60 * 1000,
   },
   trackingPassword: {
     type: String,
-    required: false,
+    required: true,
     match: /(?=.*\d)(?=.*[a-z])(?=.*[A-Z])/,
   },
-  vendorId: { type: mongoose.Schema.Types.ObjectId },
+  vendorId: mongoose.Schema.Types.ObjectId,
   // paymentId: { type: mongoose.Schema.Types.ObjectId, ref: 'paymentcards', required: true },
   paymentMethod: {
     type: String,
-    required: false,
+    required: true,
   },
 
 });
+
+
+orders.pre('save', function(next){
+  now = new Date();
+  this.updated_at = now;
+  if(!this.date) {
+      this.date = now
+  }
+  next();
+});
+
 const Orders = mongoose.model('Orders', orders);
 module.exports = Orders;
